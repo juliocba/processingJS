6caaee3227b964bf2f96b6ecb781c2e0f4ab63f3
julio baltazar <juliocesar.jcba@gmail.com>
2019-06-08 12:53:16 -0300

    adiciona todas as funções de transformações 3d

---
+++ funcoes.js
@@ -0,0 +1,33 @@
+let rotacaoX = 0;
+let rotacaoY = 0;
+let rotacaoZ = 0;
+
+let translacaoX = 0;
+let translacaoY = 0;
+let translacaoZ = 0;
+
+let escalarX = 1.5;
+let escalarY = 1.5;
+let escalarZ = 1.5;
+
+const constEscalarX = 1.5;
+const constEscalarY = 1.5;
+const constEscalarZ = 1.5;
+
+function rotacao() {
+    rotacaoX = $('#rx').val();
+    rotacaoY = $('#ry').val();
+    rotacaoZ = $('#rz').val();
+}   
+
+function translacao() {
+    translacaoX = $('#tx').val();
+    translacaoY = $('#ty').val();
+    translacaoZ = $('#tz').val();
+}
+
+function escalar() {
+    escalarX = constEscalarX + parseFloat($('#ex').val());
+    escalarY = constEscalarY + parseFloat($('#ey').val());
+    escalarZ = constEscalarZ + parseFloat($('#ez').val());
+}
--- sketch.js
+++ sketch.js
@@ -1,19 +1,28 @@
-let dim = 80.0;
-
 function setup() {
-  createCanvas(710, 400, WEBGL);
+  var cnv = createCanvas(710, 400, WEBGL);
+  var x = (windowWidth - width) / 2;
+  cnv.position(x);
+  noStroke();
 }
 
 function draw() {
-  background(100);
+  background(150);
 
-  noFill();
-  stroke(255);
+  // Orange point light on the right
+  pointLight(150, 100, 0, 500, 0, 200);
+
+  // Blue directional light from the left
+  directionalLight(0, 102, 255, -1, 0, 0);
+
+  // Yellow spotlight from the front
+  pointLight(255, 255, 109, 0, 0, 300);
+
   push();
-  translate(0, 0);
+  translate(translacaoX, translacaoY, translacaoZ);
   rotateY(rotacaoY);
   rotateX(rotacaoX);
-  box(200);
+  rotateZ(rotacaoZ);
+  scale(escalarX, escalarY, escalarZ);
+  cone(60,-150,4);
   pop();
-
 }
